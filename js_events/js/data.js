754ca38657f5c3c49610138314e2911cb39e7b3d
Samuel Cline <SamuellCline@gmail.com>
2018-02-22 17:08:45 -0700

    Added Event Listeners

---
+++ js_events/js/data.js
@@ -0,0 +1,24 @@
+
+window.addEventListener("ontouchstart", display());
+
+function display() {
+//    document.getElementById('map1').style.display = "block";
+    var x = document.getElementById("output");
+    navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+
+        var a = position.coords.latitude;
+        var b = position.coords.longitude;
+        var maploc = "";
+        maploc = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAUUNVZK5sHDkQfAFX887UEIIM7a2d3zKo&zoom=18&maptype=satellite" + '&q=' + a + ',' + b;
+        document.getElementById('map1').src = maploc;
+
+    }
+
+    document.getElementById('click').onclick = function clear(){
+
+        document.getElementById('map1').style.display = "none";
+    }
+}
